e07965090f191214afbd9e3adf04774999285fea
Adam Smallcomb <atsmallcomb@gmail.com>
2016-02-23 16:36:25 -0500

    Rename stuff

--- charge.js
+++ charge.js
@@ -1,23 +1,31 @@
 fs = require('fs');
+
+var template_dir = './_templates';
+var data_dir = './_data';
+var target_dir = './public';
+
 Mustache = require('Mustache');
-var filecontent = fs.readFileSync('data/global.json', 'utf8');
+var filecontent = fs.readFileSync(data_dir + '/global.json', 'utf8');
 globalData = JSON.parse(filecontent);
 partialViews = {};
-var files = fs.readdirSync('data');
+
+var files = fs.readdirSync(data_dir);
 for(var i in files) {
-	var filecontent = fs.readFileSync('data/'+files[i], 'utf8');
+	var filecontent = fs.readFileSync(data_dir + '/'+files[i], 'utf8');
 	globalData[files[i].split('.json')[0]] = JSON.parse(filecontent);
 }
 
-var partials = fs.readdirSync('templates/partials');
+var partials = fs.readdirSync(template_dir + '/partials');
 for(var i in partials) {
-	var filecontent = fs.readFileSync('templates/partials/'+partials[i], 'utf8');
+	var filecontent = fs.readFileSync(template_dir + '/partials/'+partials[i], 'utf8');
 	partialViews[partials[i].split('.mustache')[0]] = filecontent;
 }
-var layout = fs.readFileSync('templates/layouts/default.mustache', 'utf8'); 
-fs.readdir('templates', function(err, files){
+
+var layout = fs.readFileSync(template_dir + '/layouts/default.mustache', 'utf8'); 
+
+fs.readdir(template_dir, function(err, files){
 	files.forEach(function(file) {
-		fs.readFile('templates/'+file, 'utf8', function (err, view) {
+		fs.readFile(template_dir + '/' + file, 'utf8', function (err, view) {
 		  if (err) {
 		  	if (err.code !== 'EISDIR') {
 		  		return console.log(err.errno);
@@ -26,7 +34,7 @@ fs.readdir('templates', function(err, files){
 		  	}
 		  }
 		  partialViews.content = view;
-		  fs.writeFile('public/'+file.split('.mustache')[0]+'.html', Mustache.render(layout, globalData, partialViews), function (err) {
+		  fs.writeFile(target_dir + '/' + file.split('.mustache')[0] + '.html', Mustache.render(layout, globalData, partialViews), function (err) {
 		  	if (err) return console.log(err);
 			});
 		});
