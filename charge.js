cfa42de7ad1f62e076a4ce4239173eb9fe6a4ca8
Adam Smallcomb <atsmallcomb@gmail.com>
2016-02-23 11:09:02 -0500

    Clean up and add layouts

--- charge.js
+++ charge.js
@@ -9,19 +9,24 @@ for(var i in files) {
 	globalData[files[i].split('.json')[0]] = JSON.parse(filecontent);
 }
 
-var partials = fs.readdirSync('partials');
+var partials = fs.readdirSync('templates/partials');
 for(var i in partials) {
-	var filecontent = fs.readFileSync('partials/'+partials[i], 'utf8');
+	var filecontent = fs.readFileSync('templates/partials/'+partials[i], 'utf8');
 	partialViews[partials[i].split('.mustache')[0]] = filecontent;
 }
-
-fs.readdir('pages', function(err, files){
+var layout = fs.readFileSync('templates/layouts/default.mustache', 'utf8'); 
+fs.readdir('templates', function(err, files){
 	files.forEach(function(file) {
-		fs.readFile('pages/'+file, 'utf8', function (err, view) {
+		fs.readFile('templates/'+file, 'utf8', function (err, view) {
 		  if (err) {
-		    return console.log(err);
+		  	if (err.code !== 'EISDIR') {
+		  		return console.log(err.errno);
+		  	}else{
+		  		return;
+		  	}
 		  }
-		  fs.writeFile('public/'+file.split('.mustache')[0]+'.html', Mustache.render(view, globalData, partialViews), function (err) {
+		  partialViews.content = view;
+		  fs.writeFile('public/'+file.split('.mustache')[0]+'.html', Mustache.render(layout, globalData, partialViews), function (err) {
 		  	if (err) return console.log(err);
 			});
 		});
