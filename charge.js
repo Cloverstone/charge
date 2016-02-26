0932eb24d4bc3c001f4b8ef8381d7627e9f6f8fb
Adam Smallcomb <atsmallcomb@gmail.com>
2016-02-26 16:42:00 -0500

    Add promises

--- charge.js
+++ charge.js
@@ -1,99 +1,9 @@
-var fs = require('fs');
 var marked = require('marked');
 var http = require('http');
 var Promise = require('bluebird');
-
-
-var config = JSON.parse(fs.readFileSync('config.json', 'utf8')); 
-
-var data_dir = config.data_dir || './_site/data';
-var target_dir = config.target_dir || './public';
-var template_dir = config.template_dir || './_site/templates';
-var layouts_dir = config.layouts_dir || template_dir + '/layouts/';
-var partials_dir = config.partials_dir || template_dir + '/partials/';
-var ext = '.mustache';
-
-Mustache = require('Mustache');
-var filecontent = fs.readFileSync(data_dir + '/global.json', 'utf8');
-globalData = JSON.parse(filecontent);
-partialViews = {};
-layoutViews = {};
-var files = fs.readdirSync(data_dir);
-for(var i in files) {
-	var filecontent = fs.readFileSync(data_dir + '/'+files[i], 'utf8');
-	globalData[files[i].split('.json')[0]] = JSON.parse(filecontent);
-}
-
-// var options = {
-//   host: 'cberry.io',
-//   port: 80,
-//   path: '/d/563175329c58748ec57f5730',
-//   method: 'GET'
-// };
-// // globalData
-// http.request(options, function(res) {
-
-// 	var data = '';
-
-//   // console.log('STATUS: ' + res.statusCode);
-//   // console.log('HEADERS: ' + JSON.stringify(res.headers));
-//   res.setEncoding('utf8');
-//   res.on('data', function (chunk) {
-//     data += chunk;
-//   });  
-//   res.on('end', function () {
-//     globalData.projects = {"projects": data};
-//     console.log(globalData);
-
-//   });
-// }).end();
-
-
-
-var partials = fs.readdirSync(partials_dir);
-for(var i in partials) {
-	partialViews[partials[i].split(ext)[0]] = fs.readFileSync(partials_dir+partials[i], 'utf8');
-}
-
-var layouts = fs.readdirSync(layouts_dir);
-for(var i in layouts) {
-	layoutViews[layouts[i].split(ext)[0]] = fs.readFileSync(layouts_dir+layouts[i], 'utf8');
-}
-
-fs.readdir(template_dir, function(err, files){
-	files.forEach(function(file) {
-		fs.readFile(template_dir + '/' + file, 'utf8', function (err, view) {
-		  if (err) {
-		  	if (err.code !== 'EISDIR') {
-		  		return console.log(err.errno);
-		  	}else{
-		  		return;
-		  	}
-		  }
-		  globalData.page = globalData[file.split(ext)[0]];
-
-		  if(globalData.page.widgets){
-		  	// globalData.page.sections = [];
-		  	// globalData.page.widgets.each(function(section){
-					// globalData.page.sections.push(section[0].content)
-		  	// })
-		  }
-			partialViews.content = marked(view.split('{{>').join('{{\\>'));
-
-		  fs.writeFileSync(target_dir + '/' + file.split(ext)[0] + '.html', Mustache.render(layoutViews[ globalData.page.layout || 'default'], globalData, partialViews));
-		  delete partialViews.content;
-		});
-	})
-	console.log('Completed! - your site is located at ' + target_dir);
-})
-
-
-
-
-
-
-
-
+var _ = require('lodash');
+var fs = Promise.promisifyAll(require('fs'));
+var Mustache = require('Mustache');
 
 var PromiseRequest = Promise.method(function(options) {
     return new Promise(function(resolve, reject) { 
@@ -112,10 +22,10 @@ var PromiseRequest = Promise.method(function(options) {
                 result.body += chunk;
             });
 
-						// Resolve the promise when the response ends
-						response.on('end', function() {
-						    resolve(result);
-						});
+            // Resolve the promise when the response ends
+            response.on('end', function() {
+                resolve(result);
+            });
         });
 
         // Handle errors
@@ -129,11 +39,86 @@ var PromiseRequest = Promise.method(function(options) {
     });
 });
 
-var myRequest = PromiseRequest({
-    method: 'GET',
-    host: 'cberry.io',
-    port: 80,
-    path: '/d/563175329c58748ec57f5730',
-}).then(function(value) {
-    console.log('value:', value);
-});
+
+
+
+
+var config = JSON.parse(fs.readFileSync('config.json', 'utf8')); 
+
+var data_dir = config.data_dir || './_site/data';
+var target_dir = config.target_dir || './public';
+var template_dir = config.template_dir || './_site/templates';
+var layouts_dir = config.layouts_dir || template_dir + '/layouts/';
+var partials_dir = config.partials_dir || template_dir + '/partials/';
+var ext = '.mustache';
+
+
+
+var filecontent = fs.readFileSync(data_dir + '/global.json', 'utf8');
+globalData = JSON.parse(filecontent);
+partialViews = {};
+layoutViews = {};
+var files = fs.readdirSync(data_dir);
+for(var i in files) {
+    var filecontent = fs.readFileSync(data_dir + '/'+files[i], 'utf8');
+    globalData[files[i].split('.json')[0]] = JSON.parse(filecontent);
+}
+
+var partials = fs.readdirSync(partials_dir);
+for(var i in partials) {
+ partialViews[partials[i].split(ext)[0]] = fs.readFileSync(partials_dir+partials[i], 'utf8');
+}
+
+var layouts = fs.readdirSync(layouts_dir);
+for(var i in layouts) {
+ layoutViews[layouts[i].split(ext)[0]] = fs.readFileSync(layouts_dir+layouts[i], 'utf8');
+}
+
+fs.readdirAsync(template_dir).map(function (file) {
+
+    var ref = file.split(ext)[0];
+    var view = '';
+    return fs.readFileAsync(template_dir + "/" + file, "utf8").then(function(content){
+    	view = content;
+    	if(typeof globalData[ref].fetch !== 'undefined') {
+				return PromiseRequest(_.extend({
+				    method: 'GET',
+				    host: 'cberry.io',
+				    port: 80,
+				    path: '/d/563175329c58748ec57f5730',
+				},globalData[ref].fetch)).then(function(value) {
+				    console.log('data'+ref);
+				});
+    	}
+
+    })
+    .then(function(){
+    	// var view = gview;
+    	console.log(ref);
+      fs.writeFileSync(target_dir + '/' + ref + '.html', Mustache.render(
+      	layoutViews[ globalData[ref].layout || 'default'], 
+      	_.extend({page: globalData[ref]}, globalData), 
+      	_.extend({content: marked(view.split('{{>').join('{{\\>'))}, partialViews)
+      	
+      ));
+    }).catch(function(err){
+      if (err.code !== 'EISDIR') {
+        console.log(err.errno);
+      }
+    });
+}).then(function() {
+	console.log('Completed! - your site is located at ' + target_dir);
+})
+
+
+
+
+// var myRequest = PromiseRequest({
+//     method: 'GET',
+//     host: 'cberry.io',
+//     port: 80,
+//     path: '/d/563175329c58748ec57f5730',
+// }).then(function(value) {
+//     console.log(value.body);
+// });
+
